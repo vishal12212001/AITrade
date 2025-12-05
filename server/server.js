b4d3b418f146e1bd588df1599e71d434ac9fb4a6
vishal12212001 <35198792+vishal12212001@users.noreply.github.com>
2025-12-05 14:52:04 +0400

    git commit-m "update"
    Co-authored-by: Rahul Narota <narotaammy@gmail.com>

--- server/server.js
+++ server/server.js
@@ -19,3 +19,6 @@ app.use("/api/admin", adminRoutes);
 mongoose.connect(process.env.MONGO_URI).then(()=>console.log("MongoDB connected"));
 
 app.listen(process.env.PORT || 5000, ()=> console.log("Server running"));
+app.get("/", (req, res) => {
+  res.send("AI Trade Backend is running...");
+});
